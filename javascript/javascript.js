e530c9db2ce401840cf419bec956823fec75d131
EternalVigil <theeternalvigil@gmail.com>
2016-10-14 19:40:06 -0700

    initial commit

---
+++ javascript/javascript.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+var characters = [{
+	name: "Luke Skywalker",
+	hp: randomNum(),
+	atk: randomNum(),
+	special: ""
+}, {
+	name: "Obi Wan Kenobi",
+	hp: randomNum(),
+	atk: randomNum(),
+	special: ""
+
+}];
