b53532523cdcab49ee56dc764e7d7905d3733484
Brandon Vigil <brandonvigil@Brandons-MacBook-Pro.local>
2016-10-15 10:04:29 -0700

    javascript additions

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,13 +2,32 @@
 
 var characters = [{
 	name: "Luke Skywalker",
-	hp: randomNum(),
-	atk: randomNum(),
-	special: ""
+	hp: 100,
+	atk: 25,
+	special: "",
+	picture: "Assets/Luke.png"
 }, {
 	name: "Obi Wan Kenobi",
-	hp: randomNum(),
-	atk: randomNum(),
-	special: ""
+	hp: 120,
+	atk: 20,
+	special: "",
+	picture: "Assets/obi-wan.jpg"
 
+}, {
+	name: "Darth Sidious",
+	hp: 120,
+	atk: 30,
+	special: ""
+}, {
+	name: "Darth Maul",
+	hp: 180,
+	atk: 45,
+	special: ""
 }];
+for (var i = 0; i < characters.length; i++){
+var selectionTiles = new $("<button>");
+selectionTiles.html = characters[i].name;
+selectionTiles.data = characters[i].name;
+selectionTiles.data = ("atk", characters[i].atk);
+selectionTiles.data = ("hp", characters[i].hp);
+}
