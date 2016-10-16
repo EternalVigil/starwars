9d2472d345ecff4bd8ae145359bdb18019de8cb7
EternalVigil <theeternalvigil@gmail.com>
2016-10-15 21:56:30 -0700

    javascript additions

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -28,12 +28,29 @@ var characters = [{
 }];
 for (var i = 0; i < characters.length; i++) {
 	var selectionTiles = new $("<div>");
+	
+	var charPic = new $("<img>");
+	charPic.attr("src", characters[i].picture);
+	selectionTiles.attr("id", "char"+i);
+	selectionTiles.addClass("tiles");
+	
+	charPic.appendTo($("char"+i));
+	
+	//selectionTiles.attr("class", "characterPicture");
+	//selectionTiles.append($("<img>",{class:"characterImg" ,src:characters[i].picture}));
+	
 	console.log(characters[i].name);
-	selectionTiles.html(characters[i].name);
+	selectionTiles.data("name", characters[i].name);
+	selectionTiles.append(selectionTiles.data("name"));
+	
 	selectionTiles.data("hp", characters[i].hp);
+	selectionTiles.append("<br>" + selectionTiles.data("hp"));
 	console.log(selectionTiles.data("hp"));
+	
 	selectionTiles.data("atk", characters[i].atk);
+	selectionTiles.append("<br>" + selectionTiles.data("atk"));
 	console.log(selectionTiles.data("atk"));
+	
 	selectionTiles.data("special",characters[i].special);
 	console.log(selectionTiles.data("special"));
 	
