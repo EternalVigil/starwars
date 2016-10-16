d384ff4368c970e7df7d20968221f1bd5778cd97
EternalVigil <theeternalvigil@gmail.com>
2016-10-15 21:01:49 -0700

    HTML and JS additions

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,30 +4,39 @@ var characters = [{
 	name: "Luke Skywalker",
 	hp: 100,
 	atk: 25,
-	special: "",
+	special: "25% chance to crit for 200% damage",
 	picture: "Assets/Luke.png"
 }, {
 	name: "Obi Wan Kenobi",
 	hp: 120,
 	atk: 20,
-	special: "",
+	special: "increased attack on repetition",
 	picture: "Assets/obi-wan.jpg"
 
 }, {
 	name: "Darth Sidious",
 	hp: 120,
 	atk: 30,
-	special: ""
+	special: "every 3rd attack, add 25 damage as force lightning",
+	picture: "Assets/palpatine.jpeg"
 }, {
 	name: "Darth Maul",
 	hp: 180,
 	atk: 45,
-	special: ""
+	special: "50% increased damage on counter",
+	picture: "Assets/Maul.jpg"
 }];
-for (var i = 0; i < characters.length; i++){
-var selectionTiles = new $("<button>");
-selectionTiles.html = characters[i].name;
-selectionTiles.data = characters[i].name;
-selectionTiles.data = ("atk", characters[i].atk);
-selectionTiles.data = ("hp", characters[i].hp);
+for (var i = 0; i < characters.length; i++) {
+	var selectionTiles = new $("<div>");
+	console.log(characters[i].name);
+	selectionTiles.html(characters[i].name);
+	selectionTiles.data("hp", characters[i].hp);
+	console.log(selectionTiles.data("hp"));
+	selectionTiles.data("atk", characters[i].atk);
+	console.log(selectionTiles.data("atk"));
+	selectionTiles.data("special",characters[i].special);
+	console.log(selectionTiles.data("special"));
+	
+	
+	$("#characterSelect").append(selectionTiles);
 }
